0cb9b66d1a5d1b7d0980fa4192839e4e88868cbe
suuuinkim <suchae7@naver.com>
2022-12-28 23:29:05 +0900

    [추가] 연습문제02_대소문자 변환

--- src/main/java/practice/Exercise02.java
+++ src/main/java/practice/Exercise02.java
@@ -1,4 +1,34 @@
 package practice;
-
+import java.util.*;
 public class Exercise02 {
+
+    public String solution(String str){
+
+        String answer ="";
+        for(char c : str.toCharArray()){
+            /*
+            if(Character.isLowerCase(c)){
+                answer += Character.toUpperCase(c);
+            }else{
+                answer += Character.toLowerCase(c);
+            }
+            */
+
+//            아스키 코드를 사용한 풀이
+            if(c>=65 && c<=90){ // 대문자면
+                answer += (char)(c+32);
+            }else{
+                answer += (char)(c-32);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Exercise02 ex = new Exercise02();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(ex.solution(str));
+    }
 }
