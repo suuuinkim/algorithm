36f0713728eba5aa7751bc37fdec9108cd64c544
suuuinkim <suchae7@naver.com>
2023-01-16 16:04:35 +0900

    [추가] 섹션5 - 연습문제03_크레인 인형뽑기

---
+++ src/main/java/practice/section05/Exercise03.java
@@ -0,0 +1,46 @@
+package practice.section05;
+
+import java.util.*;
+public class Exercise03 {
+
+    public int solution(int[][] board, int[] moves){
+        int answer =0;
+        Stack<Integer> stack = new Stack<>();
+
+        for(int pos : moves){
+            for(int i=0; i<board.length; i++){
+                // System.out.println(pos + " - " + board[i][pos-1]);
+                if(board[i][pos-1] != 0){
+                    int tmp = board[i][pos-1];
+                    board[i][pos-1] = 0;
+                    if(!stack.isEmpty() && tmp == stack.peek()){
+                        answer += 2;
+                        stack.pop();
+                    }
+                    else stack.push(tmp);
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        Exercise03 ex = new Exercise03();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+        for (int i =0; i<n; i++){
+            for(int j=0; j<n; j++){
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+        for(int i=0; i<m; i++) moves[i] = sc.nextInt();
+        System.out.println(ex.solution(board, moves));
+
+
+
+    }
+}
