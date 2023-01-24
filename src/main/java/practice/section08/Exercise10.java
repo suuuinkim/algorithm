02e5410c2da1322f305eea9a500d1bccc4d56e0c
suuuinkim <suchae7@naver.com>
2023-01-24 20:56:35 +0900

    [추가] 섹션8 - 연습문제10_미로탐색(DFS)

---
+++ src/main/java/practice/section08/Exercise10.java
@@ -0,0 +1,38 @@
+package practice.section08;
+
+import java.util.*;
+public class Exercise10 {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] board;
+    static int answer;
+    public void DFS(int x, int y){
+        if(x == 7 && y ==7) answer++;
+        else{
+            for(int i=0; i<4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx >= 1 && nx<=7 && ny>=1 && ny<=7 && board[nx][ny] == 0){
+                    board[nx][ny] = 1;
+                    DFS(nx, ny);
+                    board[nx][ny] = 0;
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        Exercise10 ex = new Exercise10();
+        Scanner sc = new Scanner(System.in);
+        board = new int[8][8];
+        for(int i=1; i<=7; i++){
+            for(int j=1; j<=7; j++){
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        board[1][1] = 1;
+        ex.DFS(1, 1);
+        System.out.print(answer);
+
+    }
+}
